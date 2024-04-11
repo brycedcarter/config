490b440230b9d9176481612ecd2b097126b2810e
Bryce Carter <brycedcarter@gmail.com>
2024-04-11 10:51:47 -0700

    adding error handling for osc52

--- vim/nvim/lua/init.lua
+++ vim/nvim/lua/init.lua
@@ -390,6 +390,15 @@ end, {})
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		require("vim.ui.clipboard.osc52").copy("+")(vim.split(vim.fn.getreg(vim.v.event.regname), "\n"))
+		local status, osc52 = pcall(require, "vim.ui.clipboard.osc52")
+		if not status then
+			print(
+				"WARNING: This version of nvim is too old to support OSC52. The contents of the copy yank was not added to the system clipboard.. Please install nvim 10.0 or newer"
+			)
+		else
+			-- we are running on a new enough version of nvim to support
+			-- osc52
+			osc52.copy("+")(vim.split(vim.fn.getreg(vim.v.event.regname), "\n"))
+		end
 	end,
 })
