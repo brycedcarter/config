63b52b168462db71a05747d354cdc1138c52c418
Bryce Carter <brycedcarter@gmail.com>
2026-01-15 13:25:33 -0800

    remote tmux through dev cmd working

--- tmux/dev.sh
+++ tmux/dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This is a script that connects to tmux sessions on a remote host
+# This is a script that is used to initialize, or connect to tmux sessions both
+# remotely and locally
 
 
 ###############################################################################
@@ -63,33 +64,24 @@ do
     HOST="${POSITIONAL[0]}"
   fi
 
-#echo "all done parsing"
-#echo "local session=$LOCAL_SESSION"
-#echo "session name=$SESSION_NAME"
-#echo "host=$HOST"
-#echo $TIMEOUT
 
-if [ -n "$TMUX" ]; then
-  echo "Run this from a non-tmux shell to avoid nesting."
-  exit 1
+SSH_COMMAND="ssh -o ConnectTimeout=$TIMEOUT -o ForwardX11=yes -o ForwardX11Trusted=yes -o ForwardAgent=yes -L 8181:localhost:8181 -L 8250:localhost:8250 -L 9191:localhost:9191 -t $HOST \"$TMUX_SESSION_EXISTS $SESSION_NAME || $TMUX_NEW $SESSION_NAME; $TMUX_ATTACH $SESSION_NAME\""
+if [ -z "$TMUX" ]; then
+  # if we are not in a tmux session, then we can go ahead and connect to the remote host via ssh
+  $SSH_COMMAND
+else
+  if [ -z "$HOST" ] || [ "$HOST" = "localhost" ]; then
+    # If we are requesting a local dev session, switch the current client to it.
+    $TMUX_SESSION_EXISTS "$SESSION_NAME"
+    if [ $? -ne 0 ]; then
+      $TMUX_NEW "$SESSION_NAME"
+    fi
+    tmux switch-client -t "$SESSION_NAME"
+    exit 0
+  fi
+  # if we are in a tmux session, then we need to spawn a new terminal that is not a tmux session so that we do not end up with nested tmux sessions
+  # NOTE: we need to set TMUX=skip_auto_launch_tmux_zsh (just any nonzero string) so that ghostty does not automatically launch a new tmux session
+  nohup env TMUX=skip_auto_launch_tmux_zsh ghostty -e /bin/zsh -c "$SSH_COMMAND" >/dev/null 2>&1 & disown
 fi
 
-if [ -z "$HOST" ] || [ "$HOST" = "localhost" ]; then
-  echo "A remote host is required."
-  show_usage
-  exit 1
-fi
 
-ssh_cmd=(
-  ssh
-  -o ConnectTimeout=$TIMEOUT
-  -o ForwardX11=yes
-  -o ForwardX11Trusted=yes
-  -o ForwardAgent=yes
-  -L 8181:localhost:8181
-  -L 8250:localhost:8250
-  -L 9191:localhost:9191
-  -t "$HOST"
-  "$TMUX_SESSION_EXISTS $SESSION_NAME || $TMUX_NEW $SESSION_NAME; $TMUX_ATTACH $SESSION_NAME"
-)
-exec "${ssh_cmd[@]}"
