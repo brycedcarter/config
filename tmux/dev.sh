7aa4fb9f2c48f70859abb281be729504c42b4685
Bryce Carter <brycedcarter@gmail.com>
2026-01-15 10:52:02 -0800

    continued dev on updated tmux first env

--- tmux/dev.sh
+++ tmux/dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This is a script that is used to initialize, or connect to tmux sessions both
-# remotely and locally
+# This is a script that connects to tmux sessions on a remote host
 
 
 ###############################################################################
@@ -10,13 +9,12 @@
 
 TMUX_SESSION_EXISTS="tmux has-session -t "
 TMUX_NEW="tmux  new -d -s "
-TMUX_ATTACH="tmux -CC attach -t "
+TMUX_ATTACH="tmux attach -t "
 
 ###############################################################################
 ############## PROCESS ARGS ###################################################
 ###############################################################################
 
-LOCAL_SESSION=true
 SESSION_NAME="dev"
 HOST="localhost"
 TIMEOUT=10
@@ -25,8 +23,7 @@ TIMEOUT=10
 show_usage()
 {
   echo "dev [options] [host]
-  A helper script for creating or connecting to tmux sessions either locally 
-  or remotely
+  A helper script for connecting to tmux sessions on a remote host
 
   NOTE: if you are having issues with ssh-agent forwarding to the remote host, 
   follow this guide: https://blog.testdouble.com/posts/2016-11-18-reconciling-tmux-and-ssh-agent-forwarding/
@@ -36,8 +33,7 @@ show_usage()
              onnected to. If not provided, will default to 'dev'
     -t|--timeout SECOND: Connection timeout in seconds
     -h|--help: Show this help
-  [host]: Provide the ip address or hostname to connect to. If not provided, 
-          just use the current terminal instead of an ssh tunnel
+  [host]: Provide the ip address or hostname to connect to.
   "
 }
 
@@ -63,8 +59,7 @@ do
   if [ "${#POSITIONAL[@]}" -gt 1 ]; then
     echo "Invalid arguments"
     show_usage
-  elif [ "${#POSITIONAL[@]}" -eq 1 ] ; then 
-    LOCAL_SESSION=false
+  elif [ "${#POSITIONAL[@]}" -eq 1 ] ; then
     HOST="${POSITIONAL[0]}"
   fi
 
@@ -74,12 +69,27 @@ do
 #echo "host=$HOST"
 #echo $TIMEOUT
 
-if $LOCAL_SESSION; then 
-  $($TMUX_SESSION_EXISTS  $SESSION_NAME)
-  if [ $? -ne 0 ]; then
-    $TMUX_NEW $SESSION_NAME
-  fi 
-  $TMUX_ATTACH $SESSION_NAME
-else
-  ssh -o ConnectTimeout=$TIMEOUT -o ForwardX11=yes -o ForwardX11Trusted=yes -o ForwardAgent=yes -L 8181:localhost:8181 -L 8250:localhost:8250 -L 9191:localhost:9191 -t $HOST "$TMUX_SESSION_EXISTS $SESSION_NAME || $TMUX_NEW $SESSION_NAME; $TMUX_ATTACH $SESSION_NAME"
-fi 
+if [ -n "$TMUX" ]; then
+  echo "Run this from a non-tmux shell to avoid nesting."
+  exit 1
+fi
+
+if [ -z "$HOST" ] || [ "$HOST" = "localhost" ]; then
+  echo "A remote host is required."
+  show_usage
+  exit 1
+fi
+
+ssh_cmd=(
+  ssh
+  -o ConnectTimeout=$TIMEOUT
+  -o ForwardX11=yes
+  -o ForwardX11Trusted=yes
+  -o ForwardAgent=yes
+  -L 8181:localhost:8181
+  -L 8250:localhost:8250
+  -L 9191:localhost:9191
+  -t "$HOST"
+  "$TMUX_SESSION_EXISTS $SESSION_NAME || $TMUX_NEW $SESSION_NAME; $TMUX_ATTACH $SESSION_NAME"
+)
+exec "${ssh_cmd[@]}"
